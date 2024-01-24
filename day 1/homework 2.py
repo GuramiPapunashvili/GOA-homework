71122d9f8418efe1de520b414e7673b47c6b4e5e
GuramiPapunashvili <gurampapunashvili@mziuri.ge>
2024-01-24 17:15:07 +0400

    first commit

---
+++ day 1/homework 2.py
@@ -0,0 +1,6 @@
+name = "guram"
+surname = "Papunashvili"
+age = 15
+height = 177
+
+print("hello my name is" + " " + name + " " + "my surname is" + " " + surname + " " + "i am" + " " + str(age) + " " + "years old, my height is" + " " + str(height))
