22314e15f5b9171f7f17deb2061a8d8fa42ed557
GuramiPapunashvili <gurampapunashvili@mziuri.ge>
2024-02-15 15:55:00 +0400

    fifth commit

---
+++ day 03/lesson 03_HW.py
@@ -0,0 +1,13 @@
+mom = 46
+dad = 48
+sister = 13
+brother = 15
+
+print ("my family's age right now, mom: " + str(mom) + " dad's age:" + str(dad) + " sister's age:" + str(sister) + " brother's age:" + str(brother))
+
+mom = mom + 2
+dad = dad + 2
+sister = sister + 2
+brother = brother + 2
+
+print ("my family's age two years later, mom: " + str(mom) + " dad's age:" + str(dad) + " sister's age:" + str(sister) + " brother's age:" + str(brother))
