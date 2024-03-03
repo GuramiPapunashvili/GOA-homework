23ca28c1d056b4ac02ab911ac649e185e84cfaae
GuramiPapunashvili <gurampapunashvili@mziuri.ge>
2024-03-03 19:53:22 +0400

    eighth commit

---
+++ day 08/day 08_CW.py
@@ -0,0 +1,6 @@
+your_age = int(input("please input your age: "))
+
+if your_age >= 18:
+    print("you are an adult")
+else:
+    print("you are not an adult")    
---
+++ day 08/day 08_HW.py
@@ -0,0 +1,9 @@
+name = input("please input your name: ")
+password = input("please input your password: ")
+repeat_password = input("please repeat your password: ")
+
+if password == repeat_password:
+    print("congrats you have succesfulyl logged in")
+else:
+    print ("incorrect password, please try again")    
+      
