66511b903a24985a2c3e870aadc7257d5c149b00
GuramiPapunashvili <gurampapunashvili@mziuri.ge>
2024-03-08 18:39:12 +0400

    tenth commit

---
+++ day 10/day 10_CW.py
@@ -0,0 +1,26 @@
+age = int(input("please input your age"))
+
+if age >= 0 and age < 13:
+    print("you are a child")
+elif age >= 13 and age < 18:
+    print("you are a teenager")
+elif age >= 18 and age < 60:
+    print("you are an adult")
+else:
+    print("you are an elder") 
+
+
+
+film_list = ["harry potter", "ready player one", "hunger games", "breaking bad" ]  
+
+print(film_list)
+
+print(film_list[2])
+
+#work number 2
+number_list = [15, 27, 12, 92, 91, 2]
+print(number_list[3])
+
+#work number 3
+
+print(number_list[5], number_list[0])
