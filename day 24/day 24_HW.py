935708a2e0f366dfcef5dfcda20f8b34d33183a0
GuramiPapunashvili <gurampapunashvili@mziuri.ge>
2024-04-28 19:30:41 +0400

    twentyfourth commit

---
+++ day 24/day 24_HW.py
@@ -0,0 +1,63 @@
+#work 1 თქვენ მუშაობთ სიაზე,სადაც მოცემულია ნებისმიერი მონაცემი,
+#მაგალითად "hello", 4.14, True და ა.შ. თქვენი დავალებაა, რომ ამ სიაში მოცემული მთელი რიცხვების ჯამი დაბეჭდოთ ტერმინალში
+
+# def pos_res(arr):
+#     res = 0
+#     for i in arr:
+#         if i % 1 == 0:
+#             res = res + i
+#     return res
+
+# print(pos_res([1, 1.2, 2, 2.4, -1, True]))
+
+#work 2 თქვენ მუშაობთ რიცხვების სიაზე, შესაძლოა გქონდეთ როგორც მთელი რიცხვები, ასევე ათწილადები.
+#დავალებაა, რომ დააბრუნოთ სიის რიცხვების ჯამი, მაგრამ  ამ ჯამში არ შევა მაქსიმალური და მინიმალური მნიშვნელობები.
+
+# def min_max_non(arr):
+#     res = 0
+#     Min = min(arr)
+#     Max = max(arr)
+#     for i in arr:
+#         if i != Min and i != Max:
+#             res = res + i
+#     return res
+
+# print(min_max_non([1,2,3,4,5,2.2,3.7,15]))  
+
+#work 3 მომხმარებელს შემოატანინეთ დადებითი მთელი რიცხვი. შემდეგ გამოიყენეთ ციკლი, სიაში დაამატეთ ყველა ამ რიცხვის გამყოფი და დაბეჭდეთ ეს სია.
+
+# def num_divider(num):
+#     list = []
+#     for i in range(1,num + 1):
+#         if num % i == 0:
+#             list.append(i)
+#     return list        
+
+# print(num_divider(12))
+
+#work 4 მომხმარებელს შემოატანინეთ სიტყვა და შემდეგ განიხილეთ ამ სიტყვის ყველა ასო.
+#lowercase გადააკეთეთ uppercase-ად, ხოლო uppercase კი lowercase-ად.
+
+# def lower_upper_switch(string):
+#     res = ""
+#     for i in range(len(string)):
+#         if string[i] == string[i].lower():
+#             res =res + string[i].upper()
+#         elif string[i] == string[i].upper():
+#             res =res + string[i].lower()
+#     return res      
+
+# print(lower_upper_switch("HeLlo")) 
+
+
+#work 5 თქვენ გადმოგეცემათ მთელი რიცხვების სია. დავალებაა,
+#რომ ყველაზე ხშირად განმეორებადი რიცხვის რაოდენობა დააბრუნოთ ამ სიიდან. ამ დავალების სია აიღეთ ქვემოთ მოცემული test case-იდან.
+
+
+#...     
+
+
+
+ 
+
+
