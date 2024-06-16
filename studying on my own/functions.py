9f6c53515cd925362c8ca7e6aece4632c2fe685e
GuramiPapunashvili <gurampapunashvili@mziuri.ge>
2024-06-16 20:35:05 +0400

    thirty ninth commit

--- studying on my own/functions.py
+++ studying on my own/functions.py
@@ -1,4 +1,17 @@
+def increment_string(s):
+    L = []
+    Sum = ''
+    for i in s:
+        try:
+            int(i)
+        except:
+            pass
+        else:
+            L.append(i)
+    for i in L:
+        Sum = Sum + i        
 
-    ) 
+    return int(Sum) + 1
 
+print(increment_string("foo001"))
 
